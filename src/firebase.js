d5644f1f4d2c953ed08ca586295c33f3bf03d60c
Emma <emmarraguila@comunidad.unam.mx>
2021-11-11 22:19:07 -0600

    build: Initilize project
    Co-Authored-By: Gemma Basurto <44656020+gemminew@users.noreply.github.com>

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCsrM87t18ww1fFvzcJmrbupv8EuFaBg9A",
+  authDomain: "burger-queen-api-client.firebaseapp.com",
+  projectId: "burger-queen-api-client",
+  storageBucket: "burger-queen-api-client.appspot.com",
+  messagingSenderId: "232655338138",
+  appId: "1:232655338138:web:07c33c7f37a356dddb7d23",
+  measurementId: "G-H0FTESW7GE",
+};

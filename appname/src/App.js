263d4bfc30e7408a319d80a5b818632a5bbaa20b
Emma <emmmma2312@gmail.com>
2021-11-17 10:41:34 -0600

    Revert "1 log in page"

---
+++ appname/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from "./Login.js"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ appname/src/Dashboard.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useHistory } from "react-router";
+import "./Dashboard.css";
+import { auth, db, logout } from "./firebase";
+
+function Dashboard()

ea4a0dfcef96f31f79c2f736f626edcbcfd5f9ac
Gemma <gemminew@gmail.com>
2021-11-22 12:36:59 -0600

    Este es el bueno Emms

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Login from "./Login.jsx";
+import Dashboard from "./Dashboard.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  ); 
+}
+
+export default App;
--- src/tests/loginForm.spec.js
+++ src/tests/loginForm.spec.js
@@ -7,7 +7,8 @@ import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { LoginForm } from "../LoginForm";
 
-const handleClick = jest.fn()
+// const handleClick = jest.fn();
+const saveData = jest.fn();
 
 describe("Login should render", () => {
   test("renders Login component", () => {
@@ -30,18 +31,45 @@ describe("Labels should render", () => {
 describe("Input should be rendered", () => {
   render(<LoginForm />); // porque falla si esta dentro del test?
   test('calls onClick prop when clicked', () => {
-    render(<button onClick={handleClick}>Acceder</button>)
+    render(<button onClick={saveData}>Acceder</button>)
     fireEvent.click(screen.getByText(/Acceder/i))
-    expect(handleClick).toHaveBeenCalledTimes(1)
+    expect(saveData).toHaveBeenCalledTimes(1)
   })
 
 
   test("renders Input component", () => {
     render(<LoginForm />);
     const inputEmail = screen.getByPlaceholderText('Escribe tu correo');
+    const password = screen.getByPlaceholderText('Escribe tu contraseña');
+    const submitButton = screen.getByRole('button', { name: /Acceder/i });
+    const button
+    
     fireEvent.change(inputEmail, {
       target: { value: 'waiter@bq.com' }
     })
+
+    fireEvent.change(password, {
+      target: { value: '123456' }
+    })
+
+    expect(saveData).not.toHaveBeenCalled();
+
+    const e = { stopPropagation: jest.fn() };
+    //fireEvent.onClick(submitButton);
+    /* submitButton.simulate('click', {
+      preventDefault: () => {
+      }
+     }); */
+     const clickEvent = new MouseEvent('click');
+     Object.assign(clickEvent, {preventDefault: jest.fn()});
+   
+     fireEvent(submitButton, clickEvent);
+     console.log('que aki que')
+   
+     expect(clickEvent.preventDefault).toBeInTheDocument()
+
+    //expect(saveData).toBeInTheDocument();
+
+    expect(saveData).toHaveBeenCalledWith('waiter@bq.com', '123456');
   })
 })
-

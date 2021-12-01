5ce4585d92538717aa45ee9b1be1c3b8ed83374d
Emma <emmarraguila@comunidad.unam.mx>
2021-11-30 23:31:37 -0600

    build: add images
    Co-Authored-By: Gemma Basurto <44656020+gemminew@users.noreply.github.com>

--- src/components/Dashboard/Item.jsx
+++ src/components/Dashboard/Item.jsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
 
 export default function Item({ product, addProduct }) {
+  console.log(product)
   return (
     <div onClick={() => addProduct(product)}>
       {product && (
         <Fragment>
-          <img src={product.image} alt="Artículo" />
+          <img src={product.image} alt="Artículo"/> 
           <div className="product__info">
             <h2>{product.name}</h2>
             <p>{product.price}</p>
